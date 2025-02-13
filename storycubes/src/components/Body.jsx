2afe57700489a627a8af7351c18fadd913a8f02c
CASSIANO CALAZANS COELHO MACHADO <cassiano_c_machado@estudante.sc.senai.br>
2025-02-13 17:18:49 -0300

    Toques finais do StoryCubeOnline

--- storycubes/src/components/Body.jsx
+++ storycubes/src/components/Body.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './Body.css'
-import { FaDiceD6 } from "react-icons/fa";
+import { GiPerspectiveDiceSixFacesRandom } from "react-icons/gi";
 import { GiAngularSpider } from "react-icons/gi";
 import { GiBeech } from "react-icons/gi";
 import { GiBiceps } from "react-icons/gi";
@@ -12,6 +12,19 @@ import { GiCobra } from "react-icons/gi";
 import { GiDonkey } from "react-icons/gi";
 import { GiDuck } from "react-icons/gi";
 import { GiDrumKit } from "react-icons/gi";
+import { GiButterfly } from "react-icons/gi";
+import { GiHollowCat } from "react-icons/gi";
+import { GiNoodles } from "react-icons/gi";
+import { GiDwarfFace } from "react-icons/gi";
+import { GiNuclear } from "react-icons/gi";
+import { GiParasaurolophus } from "react-icons/gi";
+import { GiPlagueDoctorProfile } from "react-icons/gi";
+import { GiSmallFire } from "react-icons/gi";
+import { GiRingingAlarm } from "react-icons/gi";
+import { GiSuperMushroom } from "react-icons/gi";
+import { GiWolfTrap } from "react-icons/gi";
+
+
 
 
 
@@ -23,21 +36,28 @@ import "nes.css/css/nes.min.css";
 
 function Body() {
 
-    const [dado, setDado] = useState(<FaDiceD6/>)
+    const [dado, setDado] = useState(<GiPerspectiveDiceSixFacesRandom/>)
     const [arrayDadosAnteriores, setArrayDadosAnteriores] = useState([])
-    const dados = [<GiAngularSpider/>, <GiBeech/>, <GiBiceps/>, <GiBoltBomb/>, <GiBriefcase/>, <GiBurn/>, <GiCommercialAirplane/>, <GiCobra/>, <GiDonkey/>, <GiDuck/>, <GiDrumKit/>]
+    const [dados, setDados] = useState([<GiAngularSpider/>, <GiBeech/>, <GiBiceps/>, <GiBoltBomb/>, <GiBriefcase/>, <GiBurn/>, <GiCommercialAirplane/>, <GiCobra/>, <GiDonkey/>, <GiDuck/>, <GiDrumKit/>, <GiButterfly/>, <GiHollowCat/>, <GiNoodles/>, <GiDwarfFace/>, <GiNuclear/>, <GiParasaurolophus/>, <GiPlagueDoctorProfile/>, <GiSmallFire/>, <GiRingingAlarm/>, <GiSuperMushroom/>, <GiWolfTrap/>])
+    const [cont, setCont] = useState(12)
+    
 
     function gerarNovoDado(){
+        console.log(dados)
         let randomNumber = Math.floor(Math.random() * dados.length)
+        
         if (arrayDadosAnteriores.length > 3){
-
+            
             const updatedArray = [...arrayDadosAnteriores.slice(1), dados[randomNumber]]
             setArrayDadosAnteriores(updatedArray)
-
+            
         }else{
             setArrayDadosAnteriores([...arrayDadosAnteriores, dados[randomNumber]])
         }
         setDado(dados[randomNumber])
+        setDados([...dados.filter((img) => (img != dados[randomNumber]))])
+        setCont(cont - 1)
+        
         
     }
 
@@ -51,7 +71,8 @@ function Body() {
             {dado}
         </div>
         <div className="button-div">
-            <a className="nes-btn" href="#" onClick={gerarNovoDado}>Próximo Dado!</a>
+        {cont > 0 ? <a className="btn"  onClick={gerarNovoDado}> {cont} dados restantes até o fim da sua história! </a> : <button type="button" className="btn" disabled>Sua história acabou!</button> }
+            {/* <a className="nes-btn" href="#" onClick={gerarNovoDado}>{cont == 0 ? <Componente1 /> : <Componente2 /> }</a> */}
         </div>
         <div className="resultados-anteriores">
 
