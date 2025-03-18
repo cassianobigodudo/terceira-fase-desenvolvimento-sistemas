1558f42fb98514890dcf6b9215c85f9a297c98c5
cassianobigodudo <kakacassiano2010@hotmail.com>
2025-03-18 17:14:30 -0300

    Pequenas atualizações

--- pequenosprojetos_grandesfuturos/src/pages/Temperatura.jsx
+++ pequenosprojetos_grandesfuturos/src/pages/Temperatura.jsx
@@ -3,7 +3,16 @@ import './Temperatura.css'
 
 function Temperatura() {
   return (
-    <div>
+    <div className='temperatura-container'>
+        <div className="header-temperatura">
+
+            <div className="titulo-temperatura">
+
+                <label className='header-titulo'>Conversor de Temperatura</label>
+            
+            </div>
+
+        </div>
       
     </div>
   )
